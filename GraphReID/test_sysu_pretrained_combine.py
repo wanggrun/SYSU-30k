c72102c0451535707b0256a9229e19383547e5fb
Guangrun Wang (王广润) <wanggrun@mail2.sysu.edu.cn>
2020-11-27 16:09:02 +0800

    Add files via upload

---
+++ GraphReID/test_sysu_pretrained_combine.py
@@ -0,0 +1,303 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, division
+
+import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import time
+import os
+import scipy.io
+from model import ft_net, ft_net_dense, PCB, PCB_test
+
+######################################################################
+# Options
+# --------
+os.environ['CUDA_VISIBLE_DEVICES'] = '7'
+parser = argparse.ArgumentParser(description='Training')
+parser.add_argument('--gpu_ids',default='0', type=str,help='gpu_ids: e.g. 0  0,1,2  0,2')
+parser.add_argument('--which_epoch',default='last', type=str, help='0,1,2,3...or last')
+parser.add_argument('--test_dir',default='/home/zzd/Market/pytorch',type=str, help='./test_data')
+parser.add_argument('--name', default='ft_ResNet50', type=str, help='save model path')
+parser.add_argument('--batchsize', default=100, type=int, help='batchsize')
+parser.add_argument('--use_dense', action='store_true', help='use densenet121' )
+parser.add_argument('--PCB', action='store_true', help='use PCB' )
+
+opt = parser.parse_args()
+
+str_ids = opt.gpu_ids.split(',')
+#which_epoch = opt.which_epoch
+name = opt.name
+test_dir = opt.test_dir
+
+gpu_ids = []
+for str_id in str_ids:
+    id = int(str_id)
+    if id >=0:
+        gpu_ids.append(id)
+
+# set gpu ids
+if len(gpu_ids)>0:
+    torch.cuda.set_device(gpu_ids[0])
+
+######################################################################
+# Load Data
+# ---------
+#
+# We will use torchvision and torch.utils.data packages for loading the
+# data.
+#
+data_transforms = transforms.Compose([
+        transforms.Resize((256,128), interpolation=3),
+        # transforms.CenterCrop((256,128)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+############### Ten Crop        
+        #transforms.TenCrop(224),
+        #transforms.Lambda(lambda crops: torch.stack(
+         #   [transforms.ToTensor()(crop) 
+          #      for crop in crops]
+           # )),
+        #transforms.Lambda(lambda crops: torch.stack(
+         #   [transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(crop)
+          #       for crop in crops]
+          # ))
+])
+
+if opt.PCB:
+    data_transforms = transforms.Compose([
+        transforms.Resize((384,192), interpolation=3),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) 
+    ])
+
+
+data_dir = test_dir
+image_datasets = {x: datasets.ImageFolder( os.path.join(data_dir,x) ,data_transforms) for x in ['gallery','query']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batchsize,
+                                             shuffle=False, num_workers=16) for x in ['gallery','query']}
+
+class_names = image_datasets['query'].classes
+use_gpu = torch.cuda.is_available()
+
+######################################################################
+# Load model
+#---------------------------
+def load_network(network):
+    save_path = os.path.join(name,'net_%s.pth'%opt.which_epoch)
+    network.load_state_dict(torch.load(save_path))
+    return network
+
+
+######################################################################
+# Extract feature
+# ----------------------
+#
+# Extract feature from  a trained model.
+#
+def fliplr(img):
+    '''flip horizontal'''
+    inv_idx = torch.arange(img.size(3)-1,-1,-1).long()  # N x C x H x W
+    img_flip = img.index_select(3,inv_idx)
+    return img_flip
+
+def extract_feature(model,dataloaders):
+    features = torch.FloatTensor()
+    count = 0
+    for data in dataloaders:
+        img, label = data
+        n, c, h, w = img.size()
+        count += n
+        print(count)
+        if opt.use_dense:
+            ff = torch.FloatTensor(n,1024).zero_()
+        else:
+            ff = torch.FloatTensor(n,28310).zero_()
+        if opt.PCB:
+            ff = torch.FloatTensor(n,2048,6).zero_() # we have four parts
+        for i in range(2):
+            if(i==1):
+                img = fliplr(img)
+            input_img = Variable(img.cuda())
+            outputs = model(input_img) 
+        #    # outputs = torch.nn.functional.softmax(outputs,-1)
+            f = outputs.data.cpu()
+            #f = torch.nn.functional.softmax(f)
+            ff = ff+f
+        #o1 = model(Variable(img.cuda()))
+        #f1 = o1.data.cpu()
+        #fnorm = torch.norm(f1, p=2, dim=1, keepdim=True)
+        #f1 = f1.div(fnorm.expand_as(f1))
+        #img = fliplr(img)
+        #o2 = model(Variable(img.cuda()))
+        #f2 = o2.data.cpu()
+        #fnorm = torch.norm(f2, p=2, dim=1, keepdim=True)
+        #f2 = f2.div(fnorm.expand_as(f2))
+        #ff = torch.cat((f1, f2), 1)
+        # norm feature
+        if opt.PCB:
+            # feature size (n,2048,4)
+            fnorm = torch.norm(ff, p=2, dim=1, keepdim=True)
+            ff = ff.div(fnorm.expand_as(ff))
+            ff = ff.view(ff.size(0), -1)
+        else:
+            fnorm = torch.norm(ff, p=2, dim=1, keepdim=True)
+            ff = ff.div(fnorm.expand_as(ff))
+            #ff = ff
+
+        features = torch.cat((features,ff), 0)
+    return features
+
+def get_id(img_path):
+    camera_id = []
+    labels = []
+    for path, v in img_path:
+        filename = path.split('/')[-1]
+        #label = filename[0:4]
+        label = filename.split('_')[0]
+        camera = filename.split('c')[1]
+        if label[0:10]=='0000others':
+            labels.append(-1000)
+        else:
+            labels.append(int(label))
+        camera_id.append(int(camera[0]))
+    return camera_id, labels
+
+gallery_path = image_datasets['gallery'].imgs
+query_path = image_datasets['query'].imgs
+
+gallery_cam,gallery_label = get_id(gallery_path)
+query_cam,query_label = get_id(query_path)
+
+######################################################################
+# Load Collected data Trained model
+print('-------test-----------')
+if opt.use_dense:
+    model_structure = ft_net_dense(28310)
+else:
+    model_structure = ft_net(28310)
+
+if opt.PCB:
+    model_structure = PCB(28310)
+
+model_structure = nn.DataParallel(model_structure)
+model = load_network(model_structure)
+
+# Remove the final fc layer and classifier layer
+#print(model)
+if not opt.PCB:
+    model.module.model.fc = nn.Sequential()
+    # model.classifier = nn.Sequential()
+else:
+    model = PCB_test(model)
+
+print(model)
+
+# Change to test mode
+model = model.eval()
+if use_gpu:
+    model = model.cuda()
+
+# Extract feature
+gallery_feature = extract_feature(model,dataloaders['gallery'])
+query_feature = extract_feature(model,dataloaders['query'])
+
+
+import scipy.io
+import torch
+import numpy as np
+import time
+
+#######################################################################
+# Evaluate
+def evaluate(qf,ql,qc,gf,gl,gc):
+    query = qf
+    score = np.dot(gf,query)
+    # predict index
+    index = np.argsort(score)  #from small to large
+    index = index[::-1]
+    #index = index[0:2000]
+    # good index
+    query_index = np.argwhere(gl==ql)
+    camera_index = np.argwhere(gc==qc)
+
+    good_index = np.setdiff1d(query_index, camera_index, assume_unique=True)
+    junk_index1 = np.argwhere(gl==-1)
+    junk_index2 = np.intersect1d(query_index, camera_index)
+    junk_index = np.append(junk_index2, junk_index1) #.flatten())
+
+    CMC_tmp = compute_mAP(index, good_index, junk_index)
+    return CMC_tmp
+
+
+def compute_mAP(index, good_index, junk_index):
+    ap = 0
+    cmc = torch.IntTensor(len(index)).zero_()
+    if good_index.size==0:   # if empty
+        cmc[0] = -1
+        return ap,cmc
+
+    # remove junk_index
+    mask = np.in1d(index, junk_index, invert=True)
+    index = index[mask]
+
+    # find good_index index
+    ngood = len(good_index)
+    mask = np.in1d(index, good_index)
+    rows_good = np.argwhere(mask==True)
+    rows_good = rows_good.flatten()
+
+    cmc[rows_good[0]:] = 1
+    for i in range(ngood):
+        d_recall = 1.0/ngood
+        precision = (i+1)*1.0/(rows_good[i]+1)
+        if rows_good[i]!=0:
+            old_precision = i*1.0/rows_good[i]
+        else:
+            old_precision=1.0
+        ap = ap + d_recall*(old_precision + precision)/2
+
+    return ap, cmc
+
+
+
+
+query_feature = query_feature.numpy() # result['query_f']
+query_cam = np.asarray(query_cam) #result['query_cam'][0]
+query_label = np.asarray(query_label)#result['query_label'][0]
+gallery_feature = gallery_feature.numpy()#result['gallery_f']
+gallery_cam = np.asarray(gallery_cam)#result['gallery_cam'][0]
+gallery_label = np.asarray(gallery_label)#result['gallery_label'][0]
+
+
+
+CMC = torch.IntTensor(len(gallery_label)).zero_()
+ap = 0.0
+#print(query_label)
+for i in range(len(query_label)):
+    ap_tmp, CMC_tmp = evaluate(query_feature[i],query_label[i],query_cam[i],gallery_feature,gallery_label,gallery_cam)
+    if CMC_tmp[0]==-1:
+        continue
+    CMC = CMC + CMC_tmp
+    ap += ap_tmp
+    print(i, CMC_tmp[0])
+
+CMC = CMC.float()
+CMC = CMC/len(query_label) #average CMC
+print('Rank@1:%f Rank@5:%f Rank@10:%f mAP:%f'%(CMC[0],CMC[4],CMC[9],ap/len(query_label)))
+
+
+
+
+
+
+
+
+
+
